4f3d8567485eafd2742d5df9297ea64160006865
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-21 17:26:06 +0900

    add: Databinding string

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p class="text-blue">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: 'welcome vue start',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-blue {
+  color: blue;
+}
+</style>
