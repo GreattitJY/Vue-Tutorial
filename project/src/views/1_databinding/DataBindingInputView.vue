ab4251ec1b2fdbcf4ef955e5b8c0b283a69be1ee
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-21 17:33:37 +0900

    add: v-model을 활용한 양방향 데이터

---
+++ project/src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="userId" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: '양방향 데이터'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
